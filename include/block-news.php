c1fb5f5869cda1af8b4f03462974394dfe258c9e
Luckincome <jandos__91@list.ru>
2023-02-13 21:48:43 +0600

    Блок - Новости и Акции

--- include/block-news.php
+++ include/block-news.php
@@ -5,23 +5,24 @@
     
     <div id="newsticker">
         <ul>
-            <li>
-                <span>25.07.2013</span>
-                <a href="">Кредит без переплаты на планшет Samsung</a>
-                <p>С июля до сентебря во всех салонах-магазина  - планшеты Samsung в кредит без переплат!</p>
-            </li>
-            
-            <li>
-                <span>25.07.2013</span>
-                <a href="">Кредит без переплаты на планшет Samsung</a>
-                <p>С июля до сентебря во всех салонах-магазина  - планшеты Samsung в кредит без переплат!</p>
-            </li>
-            
-            <li>
-                <span>25.07.2013</span>
-                <a href="">Кредит без переплаты на планшет Samsung</a>
-                <p>С июля до сентебря во всех салонах-магазина  - планшеты Samsung в кредит без переплат!</p>
-            </li>
+            <?php
+                $result = mysql_query("SELECT * FROM news ORDER BY id DESC", $link);
+                if (mysql_num_rows($result) > 0)
+                {
+                $row = mysql_fetch_array($result);
+                do
+                {
+                  echo '
+                    <li>
+                        <span>'.$row["date"].'</span>
+                        <a href="">'.$row["title"].'</a>
+                        <p>'.$row["text"].'</p>
+                    </li>
+                  ';  
+                }
+                while ($row = mysql_fetch_array($result));
+                }
+            ?>
         </ul>
     </div>
     
