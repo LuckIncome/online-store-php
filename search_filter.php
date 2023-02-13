d1d3ac832bef09d4f256a5287da2b85edb629160
Luckincome <jandos__91@list.ru>
2023-02-13 21:46:20 +0600

    Выбор товаров по фильтру

--- search_filter.php
+++ search_filter.php
@@ -1,42 +1,14 @@
 <?php
     include("include/db_connect.php");
+    include("functions/functions.php");
 
-    $sorting = $_GET["sort"];
-    switch ($sorting) 
-    {
-        case 'price-asc';
-        $sorting = 'price ASC';
-        $sort_name = 'От дешевых к дорогим';
-        break;
-
-        case 'price-desc';
-        $sorting = 'price DESC';
-        $sort_name = 'От дорогих к дешевым';
-        break;
-
-        case 'popular';
-        $sorting = 'count DESC';
-        $sort_name = 'Популярное';
-        break;
-
-        case 'news';
-        $sorting = 'datetime DESC';
-        $sort_name = 'Новинки';
-        break;
-
-        case 'brand';
-        $sorting = 'brand';
-        $sort_name = 'От А до Я';
-        break;
-
-        default:
-        $sorting = 'products_id DESC';
-        $sort_name = 'Нет сортировки';
-        break;
-    }
+    $cat = clear_string($_GET["cat"]);
+    $type = clear_string($_GET["type"]);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="content-type" content="text/html; charset=windows-1251" />
@@ -49,47 +21,63 @@
     <script type="text/javascript" src="js/shop-script.js"></script>
     <script type="text/javascript" src="js/jquery.cookie.min.js"></script>
     <script type="text/javascript" src="trackbar/jquery.trackbar.js"></script>
-    <title>Интернет-Магазин Цифравой Техники</title>
+    <title>Поиск по параметрам</title>
 </head>
+
 <body>
-    <div id="block-body">       
+    <div id="block-body">
         <?php
         include("include/block-header.php");
-        ?>          
+        ?>
         <div id="block-right">
             <?php
                 include("include/block-category.php");
                 include("include/block-parameter.php");
                 include("include/block-news.php");
-            ?>   
-        </div>          
+            ?>
+        </div>
         <div id="block-content">
-            <div id="block-sorting">
-                <p id="nav-breadcrumbs"><a href="index.php">Главная страница</a> \ <span>Все товары</span></p>
-                <ul id="options-list">
-                    <li>Вид: </li>
-                    <li><img id="style-grid" src="images/icon-grid.png" /></li>
-                    <li><img id="style-list" src="images/icon-list.png" /></li>
-                    
-                    <li>Сортировать:</li>
-                    <li><a id="select-sort"><?php echo $sort_name; ?></a>
-                        <ul id="sorting-list">
-                            <li><a href="index.php?sort=price-asc">От дешевых к дорогим</a></li>
-                            <li><a href="index.php?sort=price-desc">От дорогих к дешевым</a></li>
-                            <li><a href="index.php?sort=popular">Популярное</a></li>
-                            <li><a href="index.php?sort=news">Новинки</a></li>
-                            <li><a href="index.php?sort=brand">От А до Я</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            
-            <ul id="block-tovar-grid">
-                <?php
-                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' ORDER BY $sorting", $link);
+            <?php
+                    if ($_GET["brand"]) 
+                    {
+                        $check_brand = implode(',', $_GET["brand"]);
+                    }
+                    $start_price = (int)$_GET["start_price"];
+                    $end_price = (int)$_GET["end_price"];
+
+                    if (!empty($check_brand) OR !empty($end_price))
+                    {
+                        if (!empty($check_brand)) $query_brand = " AND brand_id IN ($check_brand)";
+                        if (!empty($end_price)) $query_price = " AND price BETWEEN $start_price AND $end_price";
+                    }
+
+                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $query_brand $query_price ORDER BY products_id DESC", $link);
                     if (mysql_num_rows($result) > 0) 
                     {
                         $row = mysql_fetch_array($result);
+                        echo '
+                            <div id="block-sorting">
+                                <p id="nav-breadcrumbs"><a href="index.php">Главная страница</a> \ <span>Все товары</span></p>
+                                <ul id="options-list">
+                                    <li>Вид: </li>
+                                    <li><img id="style-grid" src="images/icon-grid.png" /></li>
+                                    <li><img id="style-list" src="images/icon-list.png" /></li>
+
+                                    <li>Сортировать:</li>
+                                    <li><a id="select-sort">'.$sort_name.'</a>
+                                        <ul id="sorting-list">
+                                            <li><a href="view_cat.php?'.$catlink.'type='.$type.'&sort=price-asc">От дешевых к дорогим</a></li>
+                                            <li><a href="view_cat.php?'.$catlink.'type='.$type.'&sort=price-desc">От дорогих к дешевым</a></li>
+                                            <li><a href="view_cat.php?'.$catlink.'type='.$type.'&sort=popular">Популярное</a></li>
+                                            <li><a href="view_cat.php?'.$catlink.'type='.$type.'&sort=news">Новинки</a></li>
+                                            <li><a href="view_cat.php?'.$catlink.'type='.$type.'&sort=brand">От А до Я</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <ul id="block-tovar-grid">
+                        ';
                         do 
                         {
                             if ($row["image"] != "" && file_exists("./uploads_images/".$row["image"]))
@@ -128,13 +116,12 @@
                             ';
                         }
                         while ($row = mysql_fetch_array($result));
-                    }
                 ?>
             </ul>
 
             <ul id="block-tovar-list">
                 <?php
-                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' ORDER BY $sorting", $link);
+                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $query_brand $query_price ORDER BY products_id DESC", $link);
                     if (mysql_num_rows($result) > 0) 
                     {
                         $row = mysql_fetch_array($result);
@@ -175,15 +162,19 @@
                                 </li>
                             ';
                         }
-                        while ($row = mysql_fetch_array($result));
+                            while ($row = mysql_fetch_array($result));
+                        }
+                    } else 
+                    {
+                        echo '<h3>Категория не доступна или не создана!</h3>';
                     }
                 ?>
             </ul>
-        </div>        
+        </div>
         <?php
             include("include/block-footer.php");
-        ?>          
+        ?>
     </div>
 </body>
-</html>
 
+</html>
--- view_cat.php
+++ view_cat.php
@@ -49,10 +49,12 @@
     <meta name="author" content="Helsing" />
     <link href="css/reset.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="trackbar/trackbar.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
     <script type="text/javascript" src="js/jcarousellite_1.0.1.js"></script>
     <script type="text/javascript" src="js/shop-script.js"></script>
     <script type="text/javascript" src="js/jquery.cookie.min.js"></script>
+    <script type="text/javascript" src="trackbar/jquery.trackbar.js"></script>
     <title>Интернет-Магазин Цифравой Техники</title>
 </head>
 
