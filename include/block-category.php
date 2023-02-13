816f3232f28568fffcf10304bc7d628425e1981a
Luckincome <jandos__91@list.ru>
2023-02-13 21:40:29 +0600

    Блок - категории товаров

--- include/block-category.php
+++ include/block-category.php
@@ -2,38 +2,74 @@
     <p class="header-title">Категории товаров</p>
     <ul>
         <li>
-            <a href="#">
+            <a id="index1">
                 <img src="/images/mobile-icon.gif" id="mobile-images" />Мобильные телефоны
             </a>
         
             <ul class="category-section">
                 <li><a href=""><strong>Все модели</strong> </a></li>
-                <li><a href="">Подраздел 1</a></li>
-                <li><a href="">Подраздел 2</a></li>
+                <?php
+                    $result = mysql_query("SELECT * FROM category WHERE type = 'mobile'", $link);
+                    if (mysql_num_rows($result) > 0)
+                    {
+                        $row = mysql_fetch_array($result);
+                    do
+                    {
+                        echo '
+                            <li><a href="view_cat.php?cat='.strtolower($row["brand"]).'&type='.$row["type"].'">'.$row["brand"].'</a></li>
+                        ';
+                    }
+                    while ($row = mysql_fetch_array($result));
+                    }
+                ?>
             </ul>
         </li>
         
         <li>
-            <a href="#">
+            <a id="index2">
                 <img src="/images/book-icon.gif" id="book-images" />Ноутбуки
             </a>
         
             <ul class="category-section">
                 <li><a href=""><strong>Все модели</strong> </a></li>
-                <li><a href="">Подраздел 1</a></li>
-                <li><a href="">Подраздел 2</a></li>
+                <?php
+                    $result = mysql_query("SELECT * FROM category WHERE type = 'notebook'", $link);
+                    if (mysql_num_rows($result) > 0)
+                    {
+                        $row = mysql_fetch_array($result);
+                    do
+                    {
+                        echo '
+                            <li><a href="view_cat.php?cat='.strtolower($row["brand"]).'&type='.$row["type"].'">'.$row["brand"].'</a></li>
+                        ';
+                    }
+                    while ($row = mysql_fetch_array($result));
+                    }
+                ?>
             </ul>
         </li>
         
         <li>
-            <a href="#">
+            <a id="index3">
                 <img src="/images/table-icon.gif" id="table-images" />Планшеты
             </a>
         
             <ul class="category-section">
                 <li><a href=""><strong>Все модели</strong> </a></li>
-                <li><a href="">Подраздел 1</a></li>
-                <li><a href="">Подраздел 2</a></li>
+                <?php
+                    $result = mysql_query("SELECT * FROM category WHERE type = 'notepad'", $link);
+                    if (mysql_num_rows($result) > 0)
+                    {
+                        $row = mysql_fetch_array($result);
+                    do
+                    {
+                        echo '
+                            <li><a href="view_cat.php?cat='.strtolower($row["brand"]).'&type='.$row["type"].'">'.$row["brand"].'</a></li>
+                        ';
+                    }
+                    while ($row = mysql_fetch_array($result));
+                    }
+                ?>
             </ul>
         </li>
     </ul>
