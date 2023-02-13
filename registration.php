a45ab8ae8d4287c7351e49fcb04f6df458f3cc50
Luckincome <jandos__91@list.ru>
2023-02-13 22:02:31 +0600

    Регистрация. Часть 2

--- registration.php
+++ registration.php
@@ -13,11 +13,138 @@
     <link href="css/reset.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <link href="trackbar/trackbar.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="js/jcarousellite_1.0.1.js"></script>
-    <script type="text/javascript" src="js/shop-script.js"></script>
-    <script type="text/javascript" src="js/jquery.cookie.min.js"></script>
-    <script type="text/javascript" src="trackbar/jquery.trackbar.js"></script>
+    <script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="/js/jcarousellite_1.0.1.js"></script>
+    <script type="text/javascript" src="/js/shop-script.js"></script>
+    <script type="text/javascript" src="/js/jquery.cookie.min.js"></script>
+    <script type="text/javascript" src="/trackbar/jquery.trackbar.js"></script>
+    <script type="text/javascript" src="/js/jquery.form.js"></script>
+    <script type="text/javascript" src="/js/jquery.validate.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {	
+      $('#form_reg').validate(
+				{	
+					// правила для проверки
+					rules:{
+						"reg_login":{
+							required:true,
+							minlength:5,
+                            maxlength:15,
+                            remote: {
+                            type: "post",    
+		                    url: "/reg/check_login.php"
+		                            }
+						},
+						"reg_pass":{
+							required:true,
+							minlength:7,
+                            maxlength:15
+						},
+						"reg_surname":{
+							required:true,
+                            minlength:3,
+                            maxlength:15
+						},
+						"reg_name":{
+							required:true,
+                            minlength:3,
+                            maxlength:15
+						},
+						"reg_patronymic":{
+							required:true,
+                            minlength:3,
+                            maxlength:25
+						},
+						"reg_email":{
+						    required:true,
+							email:true
+						},
+						"reg_phone":{
+							required:true
+						},
+						"reg_address":{
+							required:true
+						},
+						"reg_captcha":{
+							required:true,
+                            remote: {
+                            type: "post",    
+		                    url: "/reg/check_captcha.php"
+		                    
+		                            }
+                            
+						}
+					},
+
+					// выводимые сообщения при нарушении соответствующих правил
+					messages:{
+						"reg_login":{
+							required:"Укажите логин!",
+                            minlength:"От 5 до 15 символов!",
+                            maxlength:"От 5 до 15 символов!",
+                            remote: "Логин занят!"
+						},
+						"reg_pass":{
+							required:"Укажите пароль!",
+                            minlength:"От 7 до 15 символов!",
+                            maxlength:"От 7 до 15 символов!"
+						},
+						"reg_surname":{
+							required:"Укажите вашу Фамилию!",
+                            minlength:"От 3 до 20 символов!",
+                            maxlength:"От 3 до 20 символов!"
+						},
+						"reg_name":{
+							required:"Укажите ваше Имя!",
+                            minlength:"От 3 до 15 символов!",
+                            maxlength:"От 3 до 15 символов!"
+						},
+						"reg_patronymic":{
+							required:"Укажите ваше Отчество!",
+                            minlength:"От 3 до 25 символов!",
+                            maxlength:"От 3 до 25 символов!"
+						},
+						"reg_email":{
+						    required:"Укажите свой E-mail",
+							email:"Не корректный E-mail"
+						},
+						"reg_phone":{
+							required:"Укажите номер телефона!"
+						},
+						"reg_address":{
+							required:"Необходимо указать алрес доставки!"
+						},
+						"reg_captcha":{
+							required:"Введите код с картинки!",
+                            remote: "Не верный код проверки!"
+						}
+					},
+					
+	submitHandler: function(form){
+	$(form).ajaxSubmit({
+	success: function(data) { 
+								 
+        if (data == 'true')
+    {
+       $("#block-form-registration").fadeOut(300,function() {
+        
+        $("#reg_message").addClass("reg_message_good").fadeIn(400).html("Вы успешно зарегестрированы!");
+        $("#form_submit").hide();
+        
+       });
+         
+    }
+    else
+    {
+       $("#reg_message").addClass("reg_message_error").fadeIn(400).html(data); 
+    }
+		} 
+			}); 
+			}
+			});
+    	});
+     
+</script>
     <title>Интернет-Магазин Цифравой Техники</title>
 </head>
 
@@ -81,7 +208,7 @@
                         </li>
                         <li>
                             <div id="block-captcha">
-                                <img src="/reg/reg_captcha" />
+                                <img src="/reg/reg_captcha.php" />
                                 <input type="text" name="reg_captcha" id="reg_captcha" />
                                 <p id="reloadcaptcha">Обновить</p>
                             </div>
