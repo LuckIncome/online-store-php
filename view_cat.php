2367584ba921a605d0eb433050a0fc5b9b2062b0
Luckincome <jandos__91@list.ru>
2023-02-13 21:58:10 +0600

    Постраничная навигация товаров

--- view_cat.php
+++ view_cat.php
@@ -94,7 +94,36 @@
                         }
                     }
 
-                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting", $link);
+                    $num = 6; // Здесь указываем сколько хотим выводить товаров
+                    $page = (int)$_GET["page"]; // помещение в переменную $page значение страницы index.php?page=5
+
+                    $count = mysql_query("SELECT COUNT(*) FROM table_products WHERE visible = '1' $querycat", $link);
+                    $temp = mysql_fetch_array($count); //Запрос в базу данных определяем общее количество товаров COUNT(*)
+
+                    if ($temp[0] > 0) 
+                    {
+                        // $temp[0] обозначает что в этом значении у нас цифры
+                        // помещаем в $tempcount общее количество товаров
+                        $tempcount = $temp[0]; 
+
+                        // Находим общее число страниц
+                        $total = (($tempcount - 1) / $num) + 1; //(17 товаров - 1 / 10) + 1 = 2.6
+                        $total = intval($total); // округление 2.6 = 2 будет
+
+                        $page = intval($page); // округление index.php?page=5
+
+                        if (empty($page) or $page < 0) $page = 1;
+                        //Если index.php?page= будет пустой или index.php?page= -1, то будет index.php?page=1
+                        if ($page > $total) $page = $total;
+                        //Если index.php?page=3 будет больше 2.6, то будет index.php?page=2
+
+                        // Вычисляем начиная с какого номера следует выводить товары
+                        $start = $page * $num - $num;
+
+                        $qury_start_num = " LIMIT $start, $num";
+                    }
+
+                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting $qury_start_num", $link);
                     if (mysql_num_rows($result) > 0) 
                     {
                         $row = mysql_fetch_array($result);
@@ -164,7 +193,7 @@
 
             <ul id="block-tovar-list">
                 <?php
-                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting", $link);
+                    $result = mysql_query("SELECT * FROM table_products WHERE visible = '1' $querycat ORDER BY $sorting $qury_start_num", $link);
                     if (mysql_num_rows($result) > 0) 
                     {
                         $row = mysql_fetch_array($result);
@@ -211,8 +240,47 @@
                     {
                         echo '<h3>Категория не доступна или не создана!</h3>';
                     }
+                    echo '</ul>';
+
+                    if ($page != 1){ $pstr_prev = '<li><a class="pstr-prev" href="view_cat.php?page='.($page - 1).'">&lt;</a></li>';}
+                    if ($page != $total) $pstr_next = '<li><a class="pstr-next" href="view_cat.php?page='.($page + 1).'">&gt;</a></li>';
+
+
+                    // Формируем ссылки со страницами
+                    if($page - 5 > 0) $page5left = '<li><a href="view_cat.php?page='.($page - 5).'">'.($page - 5).'</a></li>';
+                    if($page - 4 > 0) $page4left = '<li><a href="view_cat.php?page='.($page - 4).'">'.($page - 4).'</a></li>';
+                    if($page - 3 > 0) $page3left = '<li><a href="view_cat.php?page='.($page - 3).'">'.($page - 3).'</a></li>';
+                    if($page - 2 > 0) $page2left = '<li><a href="view_cat.php?page='.($page - 2).'">'.($page - 2).'</a></li>';
+                    if($page - 1 > 0) $page1left = '<li><a href="view_cat.php?page='.($page - 1).'">'.($page - 1).'</a></li>';
+
+                    if($page + 5 <= $total) $page5right = '<li><a href="view_cat.php?page='.($page + 5).'">'.($page + 5).'</a></li>';
+                    if($page + 4 <= $total) $page4right = '<li><a href="view_cat.php?page='.($page + 4).'">'.($page + 4).'</a></li>';
+                    if($page + 3 <= $total) $page3right = '<li><a href="view_cat.php?page='.($page + 3).'">'.($page + 3).'</a></li>';
+                    if($page + 2 <= $total) $page2right = '<li><a href="view_cat.php?page='.($page + 2).'">'.($page + 2).'</a></li>';
+                    if($page + 1 <= $total) $page1right = '<li><a href="view_cat.php?page='.($page + 1).'">'.($page + 1).'</a></li>';
+
+
+                    if ($page+5 < $total)
+                    {
+                        $strtotal = '<li><p class="nav-point">...</p></li><li><a href="view_cat.php?page='.$total.'">'.$total.'</a></li>';
+                    }else
+                    {
+                        $strtotal = ""; 
+                    }
+
+                    if ($total > 1)
+                    {
+                        echo '
+                        <div class="pstrnav">
+                        <ul>
+                        ';
+                        echo $pstr_prev.$page5left.$page4left.$page3left.$page2left.$page1left."<li><a class='pstr-active' href='view_cat.php?page=".$page."'>".$page."</a></li>".$page1right.$page2right.$page3right.$page4right.$page5right.$strtotal.$pstr_next;
+                        echo '
+                        </ul>
+                        </div>
+                        ';
+                    }
                 ?>
-            </ul>
         </div>
         <?php
             include("include/block-footer.php");
